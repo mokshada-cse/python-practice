a8b799d4ea74c049e8f3bf839aba342ad2fd21a5
mokshada-cse <mokshadawaghmare1@gmail.com>
2026-02-02 16:06:41 +0530

    Create list_practice.py

---
+++ list_practice.py
@@ -0,0 +1,13 @@
+# list_practice.py
+# Author: Mokshada
+# Simple practice with lists and loops
+
+# List of favorite fruits
+fruits = ["apple", "banana", "mango", "orange"]
+
+# Print each fruit using a loop
+for fruit in fruits:
+    print("I like", fruit)
+
+# Count total fruits
+print("Total fruits:", len(fruits))
